1a93f4ed07ab23856117d04aa08124f359f5c9b0
Tynahan <nusilite@gmail.com>
2025-07-18 13:43:29 +0900

    ✨ Feat(tab): Add manage tab for instructor

--- src/layouts/instructor/InstructorSidebar.tsx
+++ src/layouts/instructor/InstructorSidebar.tsx
@@ -1,13 +1,21 @@
-import { Box } from '@mui/material';
+import { Box, Button } from '@mui/material';
 import Tab from '@mui/material/Tab';
 import Tabs from '@mui/material/Tabs';
 
 const InstructorSidebar = () => {
   return (
-    <Box sx={{scrollbarColor: "oklch( 0.4 0 0 ) transparent"}} className="overflow-y-scroll">
-      <Tabs orientation='vertical' >
-        {Array(30).fill(0).map((_, index) => <Tab sx={{ fontWeight: 600 }} key={index} value={index} label={`학생 이름${index}`} />)}
+    <Box>
+      {/* <Button className='w-full fixed'>학생 관리</Button> */}
+      <Tabs variant="fullWidth" >
+        <Tab  value="manage" label="학생 관리" />
       </Tabs>
+      
+      <Box sx={{ scrollbarColor: "oklch( 0.4 0 0 ) transparent" }} className="h-full overflow-y-scroll w-[132px] relative">
+        <Tabs orientation='vertical' value={0}>
+          {Array(30).fill(0).map((_, index) => <Tab sx={{ fontWeight: 600 }} key={index} value={index} label={`학생 이름${index}`} />)}
+        </Tabs>
+      </Box>
+
     </Box>
   )
 }
