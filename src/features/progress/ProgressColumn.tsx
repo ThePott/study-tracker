462973c36155be6f8be940b0ab3d79e215a9481e
Tynahan <nusilite@gmail.com>
2025-08-14 19:56:05 +0900

    🔥 TEMP

--- src/features/progress/ProgressColumn.tsx
+++ src/features/progress/ProgressColumn.tsx
@@ -1,9 +1,12 @@
 import { Progress } from '@/src/shared/interfaces'
 import ProgressBox from './ProgressBox'
+import { styleClassName } from '@/src/shared/constants/style'
 
-const ProgressColumn = ({ progressArray }: { progressArray: Progress[] }) => {
+const ProgressColumn = ({ bookTitle, progressArray }: { bookTitle: string, progressArray: Progress[] }) => {
+
     return (
         <div className="grow">
+            <h2 className={`${styleClassName.fontSuper} ${styleClassName.fontVivid} p-3 bg-amber-300`}>{bookTitle}</h2>
             {progressArray.map((progress) => <ProgressBox key={progress.id} progress={progress} />)}
         </div>
     )
