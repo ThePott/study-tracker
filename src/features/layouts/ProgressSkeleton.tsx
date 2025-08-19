dbcbda5634fd6fe2fd4a4929b788b2934475408f
Tynahan <nusilite@gmail.com>
2025-08-19 16:20:02 +0900

    🚸 Imprv(skeleton): Improve skeleton for progress

--- src/features/layouts/ProgressSkeleton.tsx
+++ src/features/layouts/ProgressSkeleton.tsx
@@ -1,12 +1,19 @@
 import Skeleton from '@/src/shared/components/Skeleton'
+import { styleClassName } from '@/src/shared/constants/style'
+
+const ProgressColumnSkeleton = () => {
+    return (
+        <div className={`flex flex-col items-center gap-3 ${styleClassName.memoWidth}`}>
+            <Skeleton widthInPixel={150} heightInPixel={24} />
+            <Skeleton widthInPixel={300} />
+        </div>
+    )
+}
 
 const ProgressSkeleton = () => {
-
     return (
-        <div className="flex gap-3 overflow-hidden h-full">
-            <Skeleton />
-            <Skeleton />
-            <Skeleton />
+        <div className="flex gap-3 overflow-hidden h-full mx-auto justify-center">
+            <ProgressColumnSkeleton />
         </div>
     )
 }
