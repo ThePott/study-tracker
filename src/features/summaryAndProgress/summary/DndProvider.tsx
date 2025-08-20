7fd9850ae6364deb5a249318bd0f7e46e845ab78
Tynahan <nusilite@gmail.com>
2025-08-21 07:25:40 +0900

    🐛 Fix(dnd): Sort even in diffenrent column

--- src/features/summaryAndProgress/summary/DndProvider.tsx
+++ src/features/summaryAndProgress/summary/DndProvider.tsx
@@ -18,18 +18,12 @@ const DndProvider = ({ children }: { children: ReactNode }) => {
     return activeItem
   }, [activeId])
 
-  // 현재 드래그 중인 항목의 데이터를 찾습니다
-  // const activeItem = findActiveItem()
-
-  // 드래그 시작 시 호출
   const handleDragStart = (event: DragStartEvent) => {
     const { active } = event;
     setActiveId(Number(active.id))
     setActiveBookTitle(active.data.current.bookTitle)
   }
 
-  // 드래그 오버 핸들러
-  // 항목이 드래그되면서 다른 항목이나 영역 위에 있을 때 지속적으로 호출됩니다
   const handleDragOver = (event: DragOverEvent) => {
     const { active, over } = event;
 
@@ -42,8 +36,6 @@ const DndProvider = ({ children }: { children: ReactNode }) => {
     const activeItem = findActiveItem()
     if (!activeItem) { return }
 
-    // 다른 보드 위로 드래그 중일 때
-    // 시각적 피드백을 위해 임시로 타입 변경 (실제 데이터는 handleDragEnd에서 변경됨)
     if (activeItem.inProgressStatus === overData.inProgressStatus) {
       console.log("---- same status return")
       return
@@ -57,7 +49,6 @@ const DndProvider = ({ children }: { children: ReactNode }) => {
     console.log("---- diff status:", activeItem.inProgressStatus, overData.inProgressStatus)
   }
 
-  // 드래그 종료 시 호출
   const handleDragEnd = (event: DragEndEvent) => {
     const { active, over } = event;
 
@@ -73,22 +64,6 @@ const DndProvider = ({ children }: { children: ReactNode }) => {
       return
     }
 
-    // 드래그 중인 항목 찾기
-    const activeItem = findActiveItem()
-
-    // 다른 보드로 항목 이동 (예: todo -> inprogress)
-    // over.data?.current?.type은 드롭된 보드의 타입을 나타냄 (useDroppable에서 data로 설정한 값)
-    if (activeItem.inProgressStatus !== over.data.current.inProgressStatus) {
-      // 항목의 타입을 변경하여 다른 보드로 이동
-      // ---- 이거 굳이 필요하나??? 근데 반 공간이 아니라 칸반 위로 하면 여기로 온다.
-      updateInProgressStatus(activeItem.bookTitle, activeItem.id, over.data?.current?.inProgressStatus)
-      setActiveId(null)
-      console.log("---- diff col")
-      return
-    }
-    // 동일 보드 내에서 항목 순서 변경
-    // 드래그한 항목과 드롭 위치 항목의 인덱스 찾기
-
     const activeArray = progressArrayInDict[activeBookTitle]
     if (!activeArray) {
       debugger
@@ -100,7 +75,6 @@ const DndProvider = ({ children }: { children: ReactNode }) => {
     if (activeIndex === -1) {
       debugger
       throw new Error("---- WHY NO INDEXES?")
-      // return
     }
     if (overIndex === -1) {
       overIndex = activeArray.length - 1
@@ -111,7 +85,6 @@ const DndProvider = ({ children }: { children: ReactNode }) => {
     const newProgressArrayInDict = { ...progressArrayInDict }
     newProgressArrayInDict[activeBookTitle] = newActiveArray
     setProgressArrayInDict(newProgressArrayInDict)
-
 
     setActiveId(null);
   };
