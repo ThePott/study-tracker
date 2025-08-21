205c7e6f797a7dc0c08767f8b11ffb92daf69c73
Tynahan <nusilite@gmail.com>
2025-08-21 18:42:47 +0900

    👷 CICD(s3, cloudfront): Add workflows

--- src/shared/services/neon.ts
+++ src/shared/services/neon.ts
@@ -1,8 +1,15 @@
 import axios from "axios"
 import { handleFailure, handleSuccess } from "./axiosUtils"
 
+const getBaseURL = () => {
+    if ((import.meta as any).PROD) {
+        return 'https://prod-study-tracker-api-sql-f600c502d1e4.herokuapp.com/'
+    }
+    return 'http://localhost:3456'
+}
+
 const axiosNeon  = axios.create({
-    baseURL: "http://localhost:3456",
+    baseURL: getBaseURL(),
 })
 axiosNeon.interceptors.response.use(handleSuccess, handleFailure)
 
