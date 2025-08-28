d901c12930a096501438403933479a7327e50f08
Tynahan <nusilite@gmail.com>
2025-08-28 20:21:59 +0900

    🐛 Bug fix url

--- src/shared/services/neon.ts
+++ src/shared/services/neon.ts
@@ -2,10 +2,11 @@ import axios from "axios"
 import { handleFailure, handleSuccess } from "./axiosUtils"
 
 const getBaseURL = () => {
-    if ((import.meta as any).PROD) {
-        return "https://prod-study-tracker-api-sql-f600c502d1e4.herokuapp.com/"
-    }
-    return "http://localhost:3456"
+    // if ((import.meta as any).PROD) {
+    //     return "https://prod-study-tracker-api-sql-f600c502d1e4.herokuapp.com/"
+    // }
+    // return "http://localhost:3456"
+    return "https://prod-study-tracker-api-sql-f600c502d1e4.herokuapp.com"
 }
 
 const axiosNeon = axios.create({
